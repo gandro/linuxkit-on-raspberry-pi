04c35420daf60e9979ec27c7b1ff27c35c4a9225
Sebastian Wicki <gandro@gmx.net>
2019-02-20 12:28:30 +0100

    Add Dockerfiles for emulated dockerd-arm64

---
+++ tools/dockerd-arm64/run-qemu.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+: ${KERNEL:=/dockerd/dockerd-kernel}
+: ${INITRD:=/dockerd/dockerd-initrd.img}
+: ${CMDLINE:=`cat /dockerd/dockerd-cmdline`}
+: ${CPUS:=`nproc`}
+: ${MEM:=2147483648}
+: ${MEM_LIMIT:=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes`}
+: ${PORT:=2375}
+: ${CACHE:=/cache}
+: ${CACHE_SIZE:=25G}
+
+[ -d "${CACHE}" ] || { echo "error: volume ${CACHE} does not exist!"; exit 1; }
+[ -f "${CACHE}/docker-cache.img" ] || qemu-img create -f qcow2 "${CACHE}/cache.img" "${CACHE_SIZE}"
+[ "${MEM}" -lt "${MEM_LIMIT}" ] || MEM="${MEM_LIMIT}"
+
+exec /usr/bin/qemu-system-aarch64 \
+      -smp "${CPUS}" -m `expr "${MEM}" / 1048576` -cpu cortex-a57 -machine virt \
+      -kernel "${KERNEL}" -initrd "${INITRD}" \
+      -append "${CMDLINE}" \
+      -object "rng-random,id=rng0,filename=/dev/urandom" \
+      -device "virtio-net-pci,netdev=net0" \
+      -drive "file=${CACHE}/docker-cache.img,media=disk" \
+      -netdev "user,id=net0,hostfwd=tcp::${PORT}-:2375" \
+      -nographic
