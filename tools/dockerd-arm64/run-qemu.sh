62dfe928892cd61ffb23334deea9c909465288f3
Sebastian Wicki <gandro@gmx.net>
2019-02-20 13:56:28 +0100

    dockerd-arm64: Use swapon to mount swap disk

--- tools/dockerd-arm64/run-qemu.sh
+++ tools/dockerd-arm64/run-qemu.sh
@@ -15,7 +15,7 @@
 
 [ -d "${CACHE}" ] || { echo "error: volume ${CACHE} does not exist!"; exit 1; }
 [ -f "${CACHE_FILE}" ] || qemu-img create -f qcow2 "${CACHE_FILE}" "${CACHE_SIZE}"
-[ -f "${CACHE_SWAP_FILE}"] || {fallocate -l "${CACHE_SWAP_SIZE}" ${CACHE_SWAP_FILE} && mkswap -L "swap" ${CACHE_SWAP_FILE}; }
+[ -f "${CACHE_SWAP_FILE}" ] || { fallocate -l "${CACHE_SWAP_SIZE}" ${CACHE_SWAP_FILE}; mkswap -L "swap" ${CACHE_SWAP_FILE}; }
 [ "${MEM}" -lt "${MEM_LIMIT}" ] || MEM="${MEM_LIMIT}"
 
 exec /usr/bin/qemu-system-aarch64 \
@@ -24,6 +24,6 @@ exec /usr/bin/qemu-system-aarch64 \
       -append "${CMDLINE}" \
       -device "virtio-net-pci,netdev=net0" \
       -drive "file=${CACHE_FILE},media=disk" \
-      -drive "file=${CACHE_SWAP_FILE},media=disk" \
+      -drive "file=${CACHE_SWAP_FILE},media=disk,format=raw" \
       -netdev "user,id=net0,hostfwd=tcp::${PORT}-:2375" \
       -nographic
