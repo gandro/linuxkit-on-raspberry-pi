f27b86477ea3c5bb5474d705f5c012459485bfda
Sebastian Wicki <gandro@gmx.net>
2019-03-09 22:11:08 +0100

    dockerd-arm64: Make dockerd daemon.json configurable
    By loading it as a metadata image. We also change the default BIP
    to avoid address conflicts with the outer docker daemon.

--- tools/dockerd-arm64/run-qemu.sh
+++ tools/dockerd-arm64/run-qemu.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -o errexit
 
 : ${KERNEL:=/dockerd/dockerd-kernel}
 : ${INITRD:=/dockerd/dockerd-initrd.img}
@@ -12,14 +13,24 @@
 : ${CACHE_SIZE:=25G}
 : ${CACHE_SWAP_FILE:="${CACHE}/swap.img"}
 : ${CACHE_SWAP_SIZE:=4G}
+: ${METADATA_ISO:="metadata.iso"}
+: ${DOCKER_CONF:="/etc/docker/daemon.json"}
 
+# create metadata iso file for dockerd config
+mkdir -p $(dirname "${DOCKER_CONF}")
+[ -f "${DOCKER_CONF}" ] || echo '{"bip":"172.19.0.1/16","debug":true}' > "${DOCKER_CONF}"
+jq '{"docker":{"entries":{"daemon.json":{"content":tojson}}}}' ${DOCKER_CONF} > /dockerd/config
+genisoimage -quiet -output "${METADATA_ISO}" -volid config -joliet -rock /dockerd/config
+
+# configure vm memory and disk resources
 [ -d "${CACHE}" ] || { echo "error: volume ${CACHE} does not exist!"; exit 1; }
 [ -f "${CACHE_FILE}" ] || qemu-img create -f qcow2 "${CACHE_FILE}" "${CACHE_SIZE}"
 [ -f "${CACHE_SWAP_FILE}" ] || { fallocate -l "${CACHE_SWAP_SIZE}" ${CACHE_SWAP_FILE}; mkswap -L "swap" ${CACHE_SWAP_FILE}; }
 [ "${MEM}" -lt "${MEM_LIMIT}" ] || MEM="${MEM_LIMIT}"
 
 exec /usr/bin/qemu-system-aarch64 \
-      -smp "${CPUS}" -m `expr "${MEM}" / 1048576` -cpu cortex-a57 -machine virt \
+      -smp "${CPUS}" -cpu cortex-a57 -machine virt \
+      -m `expr "${MEM}" / 1048576` \
       -kernel "${KERNEL}" -initrd "${INITRD}" \
       -append "${CMDLINE}" \
       -object rng-random,id=rng0,filename=/dev/urandom \
@@ -27,5 +38,6 @@ exec /usr/bin/qemu-system-aarch64 \
       -device "virtio-net-pci,netdev=net0" \
       -drive "file=${CACHE_FILE},media=disk" \
       -drive "file=${CACHE_SWAP_FILE},media=disk,format=raw" \
+      -drive "file=${METADATA_ISO},media=cdrom" \
       -netdev "user,id=net0,hostfwd=tcp::${PORT}-:2375" \
       -nographic
